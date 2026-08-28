fdb597f628ec6edbb762317e91fe7b8b713a791e
showqi <mshowqi12@gmail.com>
2026-08-28 07:56:59 +0600

    health route is added

--- src/server.js
+++ src/server.js
@@ -12,6 +12,10 @@ const app = express()
 //json parser 
 app.use(express.json())
 
+//health route 
+app.use("/api/health",(req,res)=>{
+    res.send("well come to the server. status 200 || ok")
+})
 
 //set routes 
 app.use("/api/email",emailWorkerRoutes)
