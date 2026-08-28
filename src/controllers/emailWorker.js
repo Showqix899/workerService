b0634e1a4e25cd8f5c9702c74e6f133d1ec3a464
showqi <mshowqi12@gmail.com>
2026-08-28 07:27:00 +0600

    comments modified

--- src/controllers/emailWorker.js
+++ src/controllers/emailWorker.js
@@ -6,21 +6,23 @@ export const defaultEmailSend = async (req, res) => {
         //get email information
         const { to, subject, text } = req.body;
 
-        //basic validations
+        //destination email check
         if (!to) {
-            return res.status(400).json({
+            return res.status(404).json({
                 message: "No Destianton Email Was Provided. Please Add A Destioantion Email"
             })
         }
 
+        //email subject check
         if (!subject) {
-            return res.status(400).json({
+            return res.status(404).json({
                 message: "Subject Is Missing. Please Provide A Subject"
             })
         }
 
+        //email body check
         if (!text) {
-            return res.status(400).json({
+            return res.status(404).json({
                 message: "Email Body Is Missing. Please Provide An Email Body"
             })
         }
